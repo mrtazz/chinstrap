8ffdfc617af1e04b8fea8503116cd3da8d611477
Daniel Schauenberg <d@unwiredcouch.com>
2011-09-09 00:34:19 +0200

    add basic test for get_char

--- tests/test_chinstrap_scanner.c
+++ tests/test_chinstrap_scanner.c
@@ -25,6 +25,21 @@ START_TEST (test_scanner_init_without_length)
 }
 END_TEST
 
+START_TEST (test_scanner_get_char)
+{
+  scanner s = scanner_init("foobar", 6);
+  scanner_token t = get_char(&s);
+  fail_unless(s.current_row == 0);
+  fail_unless(s.current_col == 1);
+  fail_unless(strcmp(s.thetemplate, "foobar"));
+  fail_unless(s.template_length == 6);
+  fail_unless(s.pos == 1);
+  fail_unless(t.row == 0);
+  fail_unless(t.col == 1);
+  fail_unless(strcmp(t.cargo, "f"));
+}
+END_TEST
+
 Suite *
 scanner_suite (void)
 {
@@ -34,6 +49,7 @@ scanner_suite (void)
   TCase *tc_core = tcase_create ("Core");
   tcase_add_test (tc_core, test_scanner_init_with_length);
   tcase_add_test (tc_core, test_scanner_init_without_length);
+  tcase_add_test (tc_core, test_scanner_get_char);
   suite_add_tcase (s, tc_core);
 
   return s;
