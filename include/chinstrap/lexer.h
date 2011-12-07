1e0bde5e6f51ffe9d3c727b86c17be040d9c2c4c
Steffen Jaeckel <s@jaeckel.eu>
2011-12-07 02:11:43 +0100

    added empty lexer.h

---
+++ include/chinstrap/lexer.h
@@ -0,0 +1,23 @@
+/**
+ *  lexer.h - header file for the chinstrap lexer module
+ */
+#ifndef __CHINSTRAP_LEXER_H__
+#define __CHINSTRAP_LEXER_H__
+
+// includes
+#include "chinstrap/types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// typedefs
+
+// API functions
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
